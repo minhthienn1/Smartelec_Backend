0393bb9ab05da8eff1381ec505f7ea9e1199f986
vThieu <se7enboiz11@gmail.com>
2026-04-24 17:42:44 +0700

    sửa lại backend cho login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class LoginDto {
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Số điện thoại không được để trống' })
   @IsString()
+  @Matches(/^0[0-9]{9}$/, { message: 'Số điện thoại phải có 10 số và bắt đầu bằng số 0' })
   phoneNumber: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
   @IsString()
   password: string;
 }
